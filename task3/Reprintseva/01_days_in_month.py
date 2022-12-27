4b6ac7bff191023181e5e72f21217f42f12a831d
rkkatya <ekaterina.reprinceva@mail.ru>
2022-12-28 01:30:10 +0300

    Reprintseva task 3.1

--- task3/Reprintseva/01_days_in_month.py
+++ task3/Reprintseva/01_days_in_month.py
@@ -13,10 +13,13 @@ print('Вы ввели', month)
 
 # TODO здесь ваш код
 a=[1,3,5,7,8,10,12]
-if month in a:
-    print(31)
-elif month==2:
-    print(28)
+if month<1 or month>12:
+    print('Номер корректен')
 else:
-    print(30)
+    if month in a:
+        print(31)
+    elif month==2:
+        print(28)
+    else:
+        print(30)
 
