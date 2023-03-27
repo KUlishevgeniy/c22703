046c17a055e607233eba8b8d05a645be27eb3f52
Anri0 <andreymolodec0708@gmail.com>
2023-03-27 21:04:33 +0300

    3

--- task 5/Adzibekov/task5.py
+++ task 5/Adzibekov/task5.py
@@ -3,8 +3,6 @@ from selenium.webdriver import Chrome
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
-# Selenium - библиотека для автоматизации действий веб браузера, скрапинга
-# запускаем браузер
 s = Service('C:\data\hrome\chromedriver.exe')
 browser = webdriver.Chrome(service=s)
 browser.get('https://www.svyaznoy.ru/search?q=%D0%B0%D0%B9%D1%84%D0%BE%D0%BD')
