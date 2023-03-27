69c556824cd311722dafad69841fab665f69fef8
Anri0 <andreymolodec0708@gmail.com>
2023-03-27 21:48:24 +0300

    4

--- task 5/Adzibekov/task5.py
+++ task 5/Adzibekov/task5.py
@@ -9,7 +9,9 @@ browser.get('https://www.svyaznoy.ru/search?q=%D0%B0%D0%B9%D1%84%D0%BE%D0%BD')
 time.sleep (10)#задержка для ввода капчи
 html_text = browser.page_source
 soup = BeautifulSoup(html_text, 'lxml')
-phones = soup.find_all('div', class_="b-product-block__name")
-prices = soup.find_all('div', class_="b-product-block__visible-price _pink")
+phones = soup.find_all('span', class_="b-product-block__name")
+prices = soup.find_all('span', class_="b-product-block__visible-price _pink")
 for phone, price in zip(phones, prices):
-    print(f"Название модели: {phone.text} | Цена: {price.text} рублей")
+    print(f"Название модели: {phone.text} | Цена: {price.text}" )
+
+
