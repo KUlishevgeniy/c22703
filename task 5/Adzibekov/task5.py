fcda9ed0ef82f577e735c2707aa8f29b979a1d36
Anri0 <andreymolodec0708@gmail.com>
2023-04-03 22:48:38 +0300

    parsing v bd

--- task 5/Adzibekov/task5.py
+++ task 5/Adzibekov/task5.py
@@ -3,6 +3,12 @@ from selenium.webdriver import Chrome
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
+import psycopg2
+
+connection = psycopg2.connect(dbname='dbdata',
+                                  user='postgres', password='Q1w2e3r4',
+                                  host='localhost')
+cursor = connection.cursor()
 
 s = Service('C:\data\hrome\chromedriver.exe')
 browser = webdriver.Chrome(service=s)
@@ -10,22 +16,26 @@ browser.get('https://www.svyaznoy.ru/search?q=%D1%80%D0%B5%D0%B4%D0%BC%D0%B8+%D0
 time.sleep (10)
 html_text = browser.page_source
 soup = BeautifulSoup(html_text, 'lxml')
+
 phones = soup.find_all('span', class_="b-product-block__name")
 prices = soup.find_all('span', class_="b-product-block__visible-price")
-#for phone, price in zip(phones, prices):
-    #print(f"Название модели: {phone.text} | Цена: {price.text}" )
-import psycopg2
-connection = psycopg2.connect(dbname='dbdata',
-                                  user='postgres', password='Q1w2e3r4',
-                                  host='localhost')
-cursor = connection.cursor()
-for phone, price in zip(phones, prices):
-            query = """ INSERT INTO public.svyaznoy(
-            "phone", "price")
-            VALUES (%s,%s);"""
-            record_to_insert = (phone.text, price.text)
-            cursor.execute(query, record_to_insert)
-connection.commit()
-cursor.close()
-connection.close()
+images = soup.find_all('img', class_="lazy-loaded")
+
+for i in range(len(images)):
+    images[i] = images[i]['src']
+
+creat_table="""create table svyaznoy
+    (id serial primary key, phone varchar(60),
+    price varchar(60),
+    image varchar(300))"""
+cursor.execute(creat_table)
+
+
+for phone, price, image in zip(phones, prices, images):
+    qwery=f"""INSERT INTO public.svyaznoy(
+	    phone, price, image)
+	    VALUES 
+	    ('{phone.text}','{price.text}','{image}')"""
+    cursor.execute(qwery)
+    connection.commit()
 
