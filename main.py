b51d66f14c0d8ea9f38fe1cba66a97e6e7a9d901
bataevs <bataev_sanal@mail.ru>
2023-04-04 01:57:02 +0300

    bataev парсинг в базу данных

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+from bs4 import BeautifulSoup
+from selenium.webdriver import Chrome
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import psycopg2
+import wget
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+connection = psycopg2.connect(host='localhost', dbname='dbdata', user='postgres', password='Q1w2e3r4')
+cursor = connection.cursor()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+s = Service('C:\data\hrome\chromedriver.exe')
+browser = webdriver.Chrome(service=s)
+browser.get('https://www.eapteka.ru/goods/vitaminy_i_bad/')
+time.sleep (10)
+html_text = browser.page_source
+soup = BeautifulSoup(html_text, 'lxml')
+
+creat_table="""create table cases
+    (id serial primary key, name varchar(150),
+    price varchar(150),
+    scr varchar(150))"""
+cursor.execute(creat_table)
+connection.commit()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+names= soup.find_all('h5', class_="listing-card__title")
+prices = soup.find_all('span', class_="listing-card__price-new")
+pictures = soup.find_all('a', class_="listing-card__link")
+for i in range(len(names)):
+    url = pictures[i].find('img')['src']
+    filename = f"C:\\Users\\Админ\\картинки\\{i}.jpg"
+    wget.download(url, filename)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    insert_qwery = """INSERT INTO public.cases(name, price, scr)
+    VALUES( %s, %s, %s);"""
+    record_to_insert = (names[i].text, prices[i].text, filename)
+    cursor.execute(insert_qwery, record_to_insert)
+    connection.commit()
+
+cursor.close()
+connection.close()
