f6a671d03aabb6285079c6fe17531d2fa2f74445
dimaklevkov <dimaklevkov@gmail.com>
2023-04-03 21:08:09 +0300

    pparsing_v_tablicy

--- task 5/Klevkov/task5.py
+++ task 5/Klevkov/task5.py
@@ -1,20 +1,43 @@
 
 from bs4 import BeautifulSoup
-from selenium.webdriver import Chrome
 from selenium import webdriver
+from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
 import time
+import psycopg2
 
+
+
+connection = psycopg2.connect(host='localhost', dbname='dbdata', user='postgres', password='Q1w2e3r4')
+cursor = connection.cursor()
 
 s = Service('C:\data\hrome\chromedriver.exe')
 browser = webdriver.Chrome(service=s)
-browser.get('https://www.mvideo.ru/product-list-page?q=%D0%B8%D0%B3%D1%80%D0%BE%D0%B2%D1%8B%D0%B5+%D0%BD%D0%BE%D1%83%D1%82%D0%B1%D1%83%D0%BA%D0%B8&category=noutbuki-118')
+browser.get('https://lcls.ru/collection/odezhda')
 time.sleep(10)
 html_text = browser.page_source
 soup = BeautifulSoup(html_text, 'lxml')
 
-laptops = soup.find_all('div', class_='product-title_text')
-prices = soup.find_all('span', class_='price_main-value')
+brands = soup.find_all('div', class_="product-preview__brand")
+titles = soup.find_all('div', class_="h2 product-preview__title")
+prices = soup.find_all('span', class_="product-preview__price-cur")
+images = soup.find_all('img', class_='lazyload product-preview__img-1 entered loaded')
 
-for laptop, price in zip(laptops,prices):
-    print(f'Модель ноутбука: {laptop.text} | Цена: {price.text} рублей')
+for i in range(len(images)):
+    images[i] = images[i]['src']
+
+creat_table="""create table clothes
+    (id serial primary key, brand varchar(60),
+    title varchar(60),
+    price varchar(60),
+    image varchar(300))"""
+cursor.execute(creat_table)
+
+
+for brand, title, price, image in zip(brands, titles, prices, images):
+    qwery=f"""INSERT INTO public.clothes(
+	    brand, title, price, image)
+	    VALUES 
+	    ('{brand.text}','{title.text}','{price.text}','{image}')"""
+    cursor.execute(qwery)
+    connection.commit()
