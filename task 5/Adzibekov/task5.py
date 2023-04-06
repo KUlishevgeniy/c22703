3ff7f9de7149305d57460f064d3f25699e7cae0c
Anri0 <andreymolodec0708@gmail.com>
2023-04-06 08:40:03 +0300

    Повторка

--- task 5/Adzibekov/task5.py
+++ task 5/Adzibekov/task5.py
@@ -4,11 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
 import psycopg2
-
-connection = psycopg2.connect(dbname='dbdata',
-                                  user='postgres', password='Q1w2e3r4',
-                                  host='localhost')
-cursor = connection.cursor()
+import wget
 
 s = Service('C:\data\hrome\chromedriver.exe')
 browser = webdriver.Chrome(service=s)
@@ -19,10 +15,18 @@ soup = BeautifulSoup(html_text, 'lxml')
 
 phones = soup.find_all('span', class_="b-product-block__name")
 prices = soup.find_all('span', class_="b-product-block__visible-price")
-images = soup.find_all('img', class_="lazy-loaded")
-
-for i in range(len(images)):
-    images[i] = images[i]['src']
+image = soup.find_all('img', class_="lazy-loaded")
+str_image=[]
+for i in range(len(image)):
+    str_image.append(image[i])
+str_image=str(str_image)
+str_image=re.findall(r'src="(.*?)"', str_image)
+for i in range(len(str_image)):
+    str_image[i]='https:'+str_image[i]
+connection = psycopg2.connect(dbname='dbdata',
+                                  user='postgres', password='Q1w2e3r4',
+                                  host='localhost')
+cursor = connection.cursor()
 
 creat_table="""create table svyaznoy
     (id serial primary key, phone varchar(60),
@@ -30,12 +34,20 @@ creat_table="""create table svyaznoy
     image varchar(300))"""
 cursor.execute(creat_table)
 
-
 for phone, price, image in zip(phones, prices, images):
     qwery=f"""INSERT INTO public.svyaznoy(
 	    phone, price, image)
 	    VALUES 
-	    ('{phone.text}','{price.text}','{image}')"""
+	    ('{phone.text}','{price.text}','{image.text}')"""
     cursor.execute(qwery)
     connection.commit()
+
+    for i in range(1, len(str_image) + 1):
+        puti = r'C:\Users\kingo\PycharmProjects\pythonProject2\TOP' + str(i) + '.jpeg'
+        qwery = f"""UPDATE public.svyaznoy
+            SET image='{puti}'
+            WHERE id={i}"""
+        cursor.execute(qwery)
+        connection.commit()
+
 
